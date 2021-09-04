b643e62379fd13ee2960ceedd41b664cda192b3b
Ashiq <AshiqAR>
2021-09-04 21:51:07 +0530

    upgraded api.py

--- websiteFiles/api.py
+++ websiteFiles/api.py
@@ -1,16 +1,24 @@
 def search():
     r = requests.get("https://api.github.com/repositories")
     json_data = json.loads(r.text)
-    li = random.choice(json_data)
-    name_repo = li["name"]
-    name_author = li["owner"]["login"]
+    repo_name_li=[]
+    repo_author_li = []
+    for i in range(3):
+        li = random.choice(json_data)
+        name_repo = li["name"]
+        name_author = li["owner"]["login"]
+
+        repo_name_li.append(name_repo)
+        repo_author_li.append(name_author)
     
-    return name_repo,name_author
+    return repo_name_li,repo_author_li
     
 import json
 import requests
 import random
 
-search()
+name,author=search()
+
+
 
 
