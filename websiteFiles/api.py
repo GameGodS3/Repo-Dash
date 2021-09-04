921d1af0c681b93c45145abf005eb4f38d142e7d
Sudev Suresh S <sudevssuresh@gmail.com>
2021-09-04 21:53:35 +0530

    Added site paths and view functions

--- websiteFiles/api.py
+++ websiteFiles/api.py
@@ -1,16 +1,13 @@
+import json
+import random
+import requests
+
+
 def search():
     r = requests.get("https://api.github.com/repositories")
     json_data = json.loads(r.text)
     li = random.choice(json_data)
     name_repo = li["name"]
     name_author = li["owner"]["login"]
-    
-    return name_repo,name_author
-    
-import json
-import requests
-import random
 
-search()
-
-
+    return name_repo, name_author
--- websiteFiles/views.py
+++ websiteFiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Function which will show the index.html file
+
+
+def index(request):
+    return render(request, 'websiteFiles/index.html')
+
+# Function which will show the result.html file
+
+
+def result(request):
+    return render(request, "websiteFiles/result.html")
